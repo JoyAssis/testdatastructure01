116a5a113e70ed42aaa310b2a9182c49e91c2715
Joy <joyceassis96@gmail.com>
2022-01-14 00:39:17 -0300

    Prática aula um

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = `Joy Assis`
+console.log(nome)
+let number = 25
+console.log(number)
+let result = 2022 - number
+console.log(result)
+console.log(result + number)
+console.log("estou mexendo no console.log")
+let quartaFeira = true
+console.log(quartaFeira)
+let typeofdata = `Boolean`
+console.log(typeofdata)
